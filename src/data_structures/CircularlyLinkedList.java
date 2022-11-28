da870570bdb9d9bba42dbc51bbd7458011a08928
Bubuka Sharif <sharifbubuka256@gmail.com>
2022-11-28 18:58:48 +0300

    feat: implemented the circularly linked list

--- src/data_structures/CircularlyLinkedList.java
+++ src/data_structures/CircularlyLinkedList.java
@@ -4,6 +4,81 @@ package data_structures;
  * for instance, used to implement round-robin scheduling
  * algorithm for allocating computational in an operating system.
  */
-public class CircularlyLinkedList {
+public class CircularlyLinkedList<T> {
+    private CircularlyLinkedList.Node<T> tail = null;
+    private int size = 0;
 
+    public CircularlyLinkedList() {}
+
+    // access methods
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public T first() {
+        if (this.isEmpty()) return null;
+        return this.tail.getNext().getElement();
+    }
+
+    public T last() {
+        if (this.isEmpty()) return null;
+        return this.tail.getElement();
+    }
+
+    // update methods
+    public void rotate() {
+        if (this.tail != null)
+            this.tail = this.tail.getNext();
+    }
+
+    public void addFirst(T element) {
+        if (this.size == 0) {
+            Node implicitHead = new Node(element, null);
+            this.tail.setNext(implicitHead);
+        } else {
+            Node<T> implicitHead = new Node(element, this.tail.getNext());
+            this.tail.setNext(implicitHead);
+        }
+        size++;
+    }
+
+    public void addLast(T element) {
+        this.addFirst(element);
+        this.tail = this.tail.getNext();
+    }
+
+    public T removeFirst() {
+        if (this.isEmpty()) return null;
+        Node<T> formerHead = this.tail.getNext();
+        if (formerHead == this.tail) this.tail = null;
+        else this.tail.setNext(formerHead.getNext());
+        this.size--;
+        return formerHead.getElement();
+    }
+
+    private static class Node<T> {
+        private T element;
+        private CircularlyLinkedList.Node<T> next;
+
+        public Node(T element, CircularlyLinkedList.Node<T> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public CircularlyLinkedList.Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(CircularlyLinkedList.Node<T> next) {
+            this.next = next;
+        }
+    }
 }
